0e4208feee61755ccfb0700ea8e4dccaec9f64b9
SebasGLR <102826@glr.nl>
2025-01-08 12:50:48 +0000

    Alle Pages gemaakt + Buttons gemaakt

--- script/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('mousemove', function(e) {
-    const stars = document.querySelector('.stars');
-    const x = (e.clientX / window.innerWidth) * 100;
-    const y = (e.clientY / window.innerHeight) * 100;
-    stars.style.transform = `translate(-${x / 10}%, -${y / 10}%)`;
-});
